e640271b508de9b3b60ebc0e5a8e1bdd2abb0c9d
Stephanie Friend <stephaniefriend91@gmail.com>
2020-01-30 13:01:29 -0700

    Refactor print and flashcard files with adding helper methods and using only the method start in runner file

--- lib/print.rb
+++ lib/print.rb
@@ -4,6 +4,13 @@ class Print
     @turn_number = 0
   end
 
+  def start(cards, round)
+    intro(cards)
+    cards.length.times { receiving_guess(cards, round) }
+    results(cards, round)
+    category_results(cards, round)
+  end
+
   def intro(cards)
     puts "Welcome! You're playing with #{cards.length} cards."
     puts "-------------------------------------------------"
@@ -14,25 +21,28 @@ class Print
   end
 
   def making_turn(cards, round)
-    puts "This is card number #{turn_number} out of #{cards.length}."
+    puts "This is card number #{turn_number()} out of #{cards.length}."
     puts "Question: #{round.current_card().question}"
+  end
 
+  def receiving_guess(cards, round)
+    making_turn(cards, round)
     puts "Guess: "
-    guess = gets.chomp
+    guess = gets.chomp.downcase
     turn = round.take_turn(guess)
     puts turn.feedback()
   end
 
-  def results(round, cards)
+  def results(cards, round)
     puts "****** Game over! ******"
-    puts "You had #{round.number_correct} correct guesses out of #{cards.length} for a total score of #{round.percent_correct.to_i}%."
+    puts "You had #{round.number_correct()} correct guesses out of #{cards.length} for a total score of #{round.percent_correct().to_i}%."
   end
 
-  def category_results(round, cards)
-    new_categories = cards.group_by do |card|
+  def category_results(cards, round)
+    sorted_category = cards.group_by do |card|
       card.category
     end
-    new_categories.keys.each do |key|
+    sorted_category.keys.each do |key|
       puts "#{key} - #{round.percent_correct_by_category(key)}% correct"
     end
   end
