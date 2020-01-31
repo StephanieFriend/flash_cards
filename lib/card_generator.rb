c39b0a4597d152a6c66a4645ed6b67944fb528a7
Stephanie Friend <stephaniefriend91@gmail.com>
2020-01-30 20:27:04 -0700

    Complete iteration 4 by reading txt file through the card generator class and passing it through the runner file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,3 +1,4 @@
+require_relative 'card'
 class CardGenerator
 
   def initialize(file_name)
@@ -5,6 +6,13 @@ class CardGenerator
   end
 
   def cards
-
+    new_cards = []
+    File.foreach(@file_name) do |line|
+      question =  line.partition(',').first
+      answer = line.partition(',').last.partition(',').first
+      category = line.partition(',').last.partition(',').last.delete("\n")
+      new_cards << Card.new(question, answer, category)
+    end
+    new_cards
   end
 end
