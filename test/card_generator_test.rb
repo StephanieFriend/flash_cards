9aa40b92cbfed9fc9e159ae07c19c94d2e84c624
Stephanie Friend <stephaniefriend91@gmail.com>
2020-01-31 20:42:54 -0700

    Remove unneeded requires in the card generator test file

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,9 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
-require './lib/round'
 require './lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
